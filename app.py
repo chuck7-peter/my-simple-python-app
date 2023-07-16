9112f7a06bf2bdfa6a53635befc85061842cb807
Chukwuemeka Peter <89167017+chuck7-peter@users.noreply.github.com>
2023-07-16 18:00:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! This is my simple python app'
+    return 'Hello, world! This is my simple python app for my hands-on project'
 
 if __name__ == '__main__':
     app.run()
